9d03aea87e9475ea8eacc0f344bc067dd9307d75
David <David@DESKTOP-JK8R7ER>
2021-04-09 15:47:34 -0300

    inicio

---
+++ src/main/java/com/example/demo/Tp1ColquiDavidEdm2021Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1ColquiDavidEdm2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1ColquiDavidEdm2021Application.class, args);
+	}
+
+}
