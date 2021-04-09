53f9f4794d7c370755380778cf1b67c46e2d11c6
David <David@DESKTOP-JK8R7ER>
2021-04-09 19:10:23 -0300

    provincias 1

---
+++ src/main/java/ar/edu/unju/edm/model/Provincia.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.edm.model;
+
+public class Provincia {
+	private int CodigoDeProvincia;
+	private String NombreDeProvincia;
+	public Provincia() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getCodigoDeProvincia() {
+		return CodigoDeProvincia;
+	}
+	public void setCodigoDeProvincia(int codigoDeProvincia) {
+		CodigoDeProvincia = codigoDeProvincia;
+	}
+	public String getNombreDeProvincia() {
+		return NombreDeProvincia;
+	}
+	public void setNombreDeProvincia(String nombreDeProvincia) {
+		NombreDeProvincia = nombreDeProvincia;
+	}
+	
+	
+	
+
+}
